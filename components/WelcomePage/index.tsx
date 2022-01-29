f4f42c8e695d26a5a8a11290600d1e8ec6532d18
azhariemuhammad <azharie_muhammad@live.com>
2022-01-29 20:52:32 +0800

    feat(invitation): add meta

--- components/WelcomePage/index.tsx
+++ components/WelcomePage/index.tsx
@@ -1,57 +1,56 @@
 import { useRouter } from 'next/router';
+import MetaHeadComponent from '../MetaHead';
 
 import styles from './welcome.module.css';
 
 interface WelcomePage {
-  guestName: string;
   isOpen: boolean;
   handleOpenInvitation: () => void;
 }
-const WelcomePage = ({
-  guestName,
-  isOpen,
-  handleOpenInvitation,
-}: WelcomePage) => {
+const WelcomePage = ({ isOpen, handleOpenInvitation }: WelcomePage) => {
   const {
     query: { to = '' },
   } = useRouter();
 
   const guest = to || '';
   return (
-    <div>
-      <div
-        className={`px-[2rem]  text-center grid h-[100vh] place-content-center ${
-          styles.modal
-        } ${isOpen ? styles.shown : ''}`}
-      >
-        <div className="mb-8">
-          <h5>KEPADA BAPAK/IBU/SAUDARA/I</h5>
-          <h5 className="text-bold">{guest}</h5>
-          <p>
-            Dengan senang hati mengundang Anda untuk hadir di hari pernikahan
-            kami
-          </p>
+    <>
+      <MetaHeadComponent />
+      <div>
+        <div
+          className={`px-[2rem]  text-center grid h-[100vh] place-content-center ${
+            styles.modal
+          } ${isOpen ? styles.shown : ''}`}
+        >
+          <div className="mb-8">
+            <h5>KEPADA BAPAK/IBU/SAUDARA/I</h5>
+            <h5 className="text-bold">{guest}</h5>
+            <p>
+              Dengan senang hati mengundang Anda untuk hadir di hari pernikahan
+              kami
+            </p>
 
-          <h1 className="text-5xl font-bold font-sacramento text-white text-center mt-5">
-            Musliha
-          </h1>
-          <h1 className="text-5xl font-bold font-sacramento text-white text-center">
-            &amp;
-          </h1>
-          <h1 className="text-5xl font-bold font-sacramento text-white text-center">
-            Afwan
-          </h1>
-        </div>
-        <div className="mt-8">
-          <button
-            className="rounded-full bg-rose-800 text-white h-[32px] w-[170px]"
-            onClick={handleOpenInvitation}
-          >
-            Buka Undangan
-          </button>
+            <h1 className="text-5xl font-bold font-sacramento text-white text-center mt-5">
+              Musliha
+            </h1>
+            <h1 className="text-5xl font-bold font-sacramento text-white text-center">
+              &amp;
+            </h1>
+            <h1 className="text-5xl font-bold font-sacramento text-white text-center">
+              Afwan
+            </h1>
+          </div>
+          <div className="mt-8">
+            <button
+              className="rounded-full bg-rose-800 text-white h-[32px] w-[170px]"
+              onClick={handleOpenInvitation}
+            >
+              Buka Undangan
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default WelcomePage;
