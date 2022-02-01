d76fd313aa7ba47f1e5240ddfb09d246da1a17a7
azhariemuhammad <azharie_muhammad@live.com>
2022-02-01 15:56:17 +0800

    update meta

--- components/MetaHead/index.tsx
+++ components/MetaHead/index.tsx
@@ -6,20 +6,20 @@ const MetaHeadComponent = () => {
       <title>Undangan Pernikahan Musliha &amp; Afwan</title>
       <meta
         property="og:image"
-        content="https://user-images.githubusercontent.com/19600009/151661375-27a7bc87-12f7-4d00-94e3-e1648e63c4fc.jpg"
+        content="https://user-images.githubusercontent.com/19600009/151924036-f7e90cf6-955d-4c07-8a77-a8bb32a00bb3.jpg"
       />
       <meta
         property="og:title"
-        content="Undangan Pernikahan Musliha &amp; Afwan"
+        content="Undangan Pernikahan Fatma &amp; Afwan"
       />
       <meta
         property="og:description"
-        content="Kediaman Mempelai Wanita | 15 Januari 2022"
+        content="Bertempat di Kediaman Mempelai Pria | 22 Februari 2022"
       />
       <meta property="og:type" content="website" />
       <meta
         property="og:url"
-        content="https://nikahhariini.vercel.app/muslihaafwan"
+        content="https://nikahhariini.vercel.app/afwanfatma"
       ></meta>
     </Head>
   );
