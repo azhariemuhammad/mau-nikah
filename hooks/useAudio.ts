063fd03508eae049165cf6a62feb46d027b7829e
azhariemuhammad <azharie_muhammad@live.com>
2022-02-18 06:30:54 +0800

    add server side

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -1,16 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useAudio = (url: string) => {
-  const [audio] = useState(new Audio(url));
+  const [audio, setAudio] = useState<HTMLAudioElement>();
   const [playing, setPlaying] = useState(true);
 
   const toggle = () => setPlaying(!playing);
 
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  }, [playing]);
+  const player = useCallback(() => {
+    if (!audio) {
+      console.log({ audio, url });
+      setAudio(new Audio(url || ''));
+    }
+    if (audio) {
+      return playing ? audio.play() : audio.pause();
+    }
+  }, [audio, playing]);
+  // @ts-ignore
+  useEffect(() => player(), [player]);
 
   useEffect(() => {
+    if (!audio) return;
     audio.addEventListener('ended', () => setPlaying(false));
     return () => {
       audio.removeEventListener('ended', () => setPlaying(false));
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,35 +1,65 @@
-import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import styles from '@/styles/Home.module.css';
+import getWeddingBySlug from 'models/getWeddings';
 
 import wedFetcher from '@/utils/wedFetch';
 
 import Invitation from '@/components/Containers/Invitation';
 import LoaderLove from '@/components/Loader';
+import { Wedding } from '@/components/Containers/interface';
 
-const InvitationPage: NextPage = () => {
+interface InvitationResponse {
+  response: {
+    error: string;
+    data: Array<Wedding>;
+    count: number;
+    status: number;
+    statusText: string;
+    body: Array<Wedding>;
+  };
+}
+const InvitationPage = ({ response }: InvitationResponse) => {
   const router = useRouter();
-  const slug = router?.query?.slug || '';
-  console.log({ router });
+  // const slug = router?.query?.slug || '';
+  // console.log({ router });
 
-  const { data: response, error } = useSWR(
-    slug ? `/api/wedding/${slug}` : null,
-    wedFetcher
-  );
+  // const { data: response, error } = useSWR(
+  //   slug ? `/api/wedding/${slug}` : null,
+  //   wedFetcher
+  // );
   const { data = [] } = response || {};
-  console.log({ data, error });
-  if (!data.length) {
-    return (
-      <div className="grid place-content-center h-screen max-w-[640px] m-auto">
-        <h2 className="text-3xl text-center text-fuchsia-500 font-sacramento">
-          NikahHariIni
-        </h2>
-        <LoaderLove />
-      </div>
-    );
-  }
+  console.log({ response });
+
+  console.log({ data: data[0] });
+
+  // console.log({ data, error });
+  // if (!data.length) {
+  //   return (
+  //     <div className="grid place-content-center h-screen max-w-[640px] m-auto">
+  //       <h2 className="text-3xl text-center text-fuchsia-500 font-sacramento">
+  //         NikahHariIni
+  //       </h2>
+  //       <LoaderLove />
+  //     </div>
+  //   );
+  // }
   return <>{data.length && <Invitation weddData={data[0]} />}</>;
 };
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+export async function getServerSideProps({ params }: Props) {
+  const response = await getWeddingBySlug(params.slug);
+
+  return {
+    props: {
+      response,
+    },
+  };
+}
 
 export default InvitationPage;
