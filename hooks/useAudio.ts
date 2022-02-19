143f58a9231b3ac11b85678a97e17f668726875a
azhariemuhammad <azharie_muhammad@live.com>
2022-02-19 22:10:53 +0800

    add audio tag

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -1,17 +1,18 @@
 import { useCallback, useEffect, useState } from 'react';
 import { AUDIO_1 } from 'constants/index.js';
 const useAudio = (url: string) => {
-  const [audio, setAudio] = useState<HTMLAudioElement>();
+  // const [audio, setAudio] = useState<HTMLAudioElement>();
   const [playing, setPlaying] = useState(true);
 
   const toggle = () => setPlaying(!playing);
 
   const player = useCallback(() => {
-    if (!audio) {
-      setAudio(new Audio(AUDIO_1 || ''));
-    }
+    const audio = document.getElementById('myAudio') as HTMLAudioElement;
+    // if (!audio) {
+    //   setAudio(new Audio(AUDIO_1 || ''));
+    // }
     if (audio) {
-      audio.load();
+      // audio.load();
       return playing
         ? audio
             ?.play()
@@ -19,11 +20,12 @@ const useAudio = (url: string) => {
             .catch((e) => alert(e))
         : audio?.pause();
     }
-  }, [audio, playing]);
+  }, [playing]);
   // @ts-ignore
   useEffect(() => player(), [player]);
 
   useEffect(() => {
+    const audio = document.getElementById('myAudio') as HTMLAudioElement;
     if (!audio) return;
     audio.addEventListener('ended', () => setPlaying(false));
     return () => {
