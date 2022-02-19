122b4d2a1007c8a97ee9cd8bed24c1d6fb40205e
azhariemuhammad <azharie_muhammad@live.com>
2022-02-19 21:54:47 +0800

    change audio

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -1,23 +1,27 @@
 import { useCallback, useEffect, useState } from 'react';
-
+import { AUDIO_1 } from 'constants/index.js';
 const useAudio = (url: string) => {
-  const [audio, setAudio] = useState<HTMLAudioElement>();
+  const [audio, setAudio] = useState(AUDIO_1);
   const [playing, setPlaying] = useState(true);
 
   const toggle = () => setPlaying(!playing);
 
   const player = useCallback(() => {
     if (!audio) {
+      // @ts-ignore
       setAudio(new Audio(url || ''));
     }
     if (audio) {
+      // @ts-ignore
       audio.load();
       return playing
         ? audio
+            // @ts-ignore
             ?.play()
             .then(() => console.log('Playback resumed successfully'))
-            .catch((e) => alert(e))
-        : audio?.pause();
+            .catch((e: unknown) => alert(e))
+        : // @ts-ignore
+          audio?.pause();
     }
   }, [audio, playing]);
   // @ts-ignore
